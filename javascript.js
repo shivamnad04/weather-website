98ab7574fcc21b1fff1a63633950984f40fb654a
shivamnad04 <145188615+shivamnad04@users.noreply.github.com>
2025-06-09 00:19:20 +0530

    REMOVING API KEY

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 
-const apiKey = "8090888fee810a2643db1a53bb2b9e2f";
+const apiKey = YOUR_API_KEY;
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
